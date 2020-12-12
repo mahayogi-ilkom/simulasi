bc1e5853a1960158b32d5ff2fcd2841727d7bb7a
mahayogi-ilkom <75881966+mahayogi-ilkom@users.noreply.github.com>
2020-12-12 18:24:28 +0800

    Add files via upload

---
+++ prediksi_permintaan.php
@@ -0,0 +1,76 @@
+<html>
+	<head>
+		<title>PROGRAM SIMULASI MONTE CARLO</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/main.css">
+	  	<link rel="stylesheet" href="css/bootstrap.min.css">
+	  	<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="icon" href="img/favicon.ico">
+	  	<script src="js/jquery.js"></script>
+	  	<script src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+					<center> <h1>Prediksi Permintaan (Monte Carlo)</h1> </center>
+			</div>
+		</div>
+	</nav>
+
+
+		<div class="panel panel-primary">
+					<center> Input Data Permintaan </center>
+						<div class="panel-body">
+							<?php if(empty($_POST)): ?>
+							<div class="input-group">
+								<form action="prediksi_permintaan.php" method="post">
+									<table class="custom-padding-table">
+										<tr>
+											<td>Masukan jumlah data</td>
+											<td>:</td>
+											<td><input type="number" min="0" name="jumlah" class="form-control" required="" oninvalid="this.setCustomValidity('Harap di isi !')" oninput="setCustomValidity('')"></td>
+											<div class="input-group-btn">
+											<td><input type="submit" value="Ok" class="btn btn-success"></td>
+      										</div>
+									</table>
+								</form>
+							</div>
+							<?php else: ?>
+								<?php $banyak = $_POST['jumlah'];?>
+									<?php if(!empty($banyak)): ?>
+									<div class="input-group">
+									<h1><center>Tahap 1</center></h1>
+									<p><center>Silahkan masukan data permintaan</center></p>
+									  <form action="proses_prediksi_permintaan.php" method="post">
+									  	<div class="table table-responsive">
+										  <table class="table table-hover custom-table-header">
+												  <tr>
+													<th>Penjualan / minggu</th>
+													<th>Jumlah minggu</th>
+												  <tr>
+											<?php for($i=0; $i<$banyak; $i++): ?>
+												  <tr>
+													  <td><input type=number min=0 name=demand[] placeholder="0" class="form-control" required="" oninvalid="this.setCustomValidity('Harap di isi !')" oninput="setCustomValidity('')"></td>
+													  <td><input type=number min=1 name=freq[] placeholder="0" class="form-control" required="" oninvalid="this.setCustomValidity('Harap di isi !')" oninput="setCustomValidity('')"></td>
+												  </tr>
+											<?php endfor; ?>
+										  </table>
+										  
+										  <div class="input-group-btn">
+											<input type="hidden" name="jumlah" value="<?php echo $banyak; ?>">
+											<center><input type="submit" value="Hitung" name="submit" class="btn btn-success" style="padding-left: 30px; padding-right: 30px;"></center>
+										  </div>
+										</div>
+									  </form>
+									</div>
+									<?php endif; ?>
+							<?php endif; ?>
+									</div>
+						</div>
+					</div>
+
+	</div>
+	</body>
+
+</html>
